a1341aec88201c6025f73a37b8a37f120079ccd7
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 12:40:03 +0300

    Update RecipientPage.js

---
+++ src/pages/RecipientPage.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import RecipientProfile from '../components/recipient/RecipientProfile';
+import RequestBlood from '../components/recipient/RequestBlood';
+import MatchResults from '../components/recipient/MatchResults';
+import '../../App.css';
+
+const RecipientPage = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user || user.role !== 'recipient') {
+    return <p>Unauthorized</p>;
+  }
+
+  return (
+    <div className="page">
+      <h1>Recipient Portal</h1>
+      <RecipientProfile />
+      <RequestBlood />
+      <MatchResults />
+    </div>
+  );
+};
+
+export default RecipientPage;
