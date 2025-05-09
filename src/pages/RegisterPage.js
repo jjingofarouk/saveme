a40fa908c8a1d039ad41e0ecb3ad66fed139cdf2
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 12:38:59 +0300

    Update RegisterPage.js

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Register from '../components/auth/Register';
+import Modal from '../components/common/Modal';
+import '../../App.css';
+
+const RegisterPage = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="page">
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Register onClose={() => setIsOpen(false)} />
+      </Modal>
+    </div>
+  );
+};
+
+export default RegisterPage;
