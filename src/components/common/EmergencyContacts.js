846b3c306282d19bbb789971db98133e7bdc9666
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-10 02:39:22 +0300

    Update EmergencyContacts.js

---
+++ src/components/common/EmergencyContacts.js
@@ -0,0 +1,99 @@
+export const contacts = [
+    {
+      name: 'AAR Health Services',
+      address: 'Plot 16A, Elizabeth Avenue, Upper Kololo',
+      contact: ['0716166999', '0757166999', '0414258527', '0414560900'],
+    },
+    {
+      name: 'Bai Health & Medical Intin Centre',
+      address: 'Rashid Khamis Road, Old Kampala',
+      contact: ['0414255700'],
+    },
+    {
+      name: 'Bugolobi Nursing Home Ambulance Services',
+      address: 'Plot 34/6, Spring Road, Bugolobi',
+      contact: ['0414222354'],
+    },
+    {
+      name: 'Care Plus Emergency Medical Services',
+      address: 'Plot 135, Kira Road, Kamwokya',
+      contact: ['0773395395', '0706395395'],
+    },
+    {
+      name: 'Case Medical Centre',
+      address: 'Plot 69/71, Buganda Road',
+      contact: ['0312250362'],
+    },
+    {
+      name: 'City Ambulance Limited',
+      address: 'Plot 12, Acacia Avenue, Kololo (Opp. FINCA Head office)',
+      contact: ['0414240158', '0392177174'],
+      tollFree: '0800111044',
+      email: 'info@cityambulance.net',
+    },
+    {
+      name: 'International Air Ambulance',
+      address: 'Kitgum House, Jinja Road',
+      contact: ['0414344374'],
+    },
+    {
+      name: 'International Hospital Kampala',
+      address: 'Plot 4686, Kisugu, Namuwongo',
+      contact: ['0312200400'],
+    },
+    {
+      name: 'Rescue Medical Services',
+      address: 'Mobile Service',
+      contact: ['0772217218', '0772349990'],
+      email: 'rmsuganda@gmail.com',
+    },
+    {
+      name: 'St. John\'s Ambulance Services',
+      address: 'Plot 29, Buganda Road, (next to Baden Powell House)',
+      contact: ['0414230671', '0414236007', '0772503296'],
+      email: 'stjohnug@utionline.co.ug',
+    },
+    {
+      name: 'Samaritan Ambulance Transport & Community',
+      address: 'Katalemwa Road, Naguru, Nakawa',
+      contact: ['0414288229'],
+    },
+    {
+      name: 'The Surgery',
+      address: 'Plot 42, Naguru Drive, Naguru',
+      contact: ['0752756003', '0312256001', '0312256002', '0312256003'],
+    },
+    {
+      name: 'Uganda Ambulance Service',
+      address: 'Mobile Service',
+      contact: ['0782556878'],
+      emergencyContact: ['0792112999', '0712112999'],
+      email: 'ugambula@yahoo.co.uk',
+    },
+    {
+      name: 'Uganda Charity Ambulance And Rescue Services (UCAARS)',
+      address: 'Ntinda Shopping Center, Room D13',
+      contact: ['0704493913'],
+    },
+    {
+      name: 'Uganda Police Ambulance Service',
+      address: 'Various Locations',
+      contact: ['0414226031'],
+    },
+    {
+      name: 'Ambulance Services Uganda',
+      address: 'Kampala, Uganda',
+      contact: ['0774123456', '0414567890'],
+    },
+    {
+      name: 'Life Care Ambulance Services',
+      address: 'Bwaise, Kampala',
+      contact: ['0701234567', '0754567890'],
+    },
+    {
+      name: 'Royal Ambulance',
+      address: 'Wandegeya, Kampala',
+      contact: ['0773234567', '0312675890'],
+    },
+  ];
+  
