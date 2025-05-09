7a80f835a92780b337f2c20e2f5bac93ec61c8f6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 12:40:37 +0300

    Update NotFound.js

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../App.css';
+
+const NotFound = () => (
+  <div className="page">
+    <h1>404 - Page Not Found</h1>
+    <Link to="/">Go Home</Link>
+  </div>
+);
+
+export default NotFound;
