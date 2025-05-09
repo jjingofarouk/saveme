1088b35f8c29399ac2394e2ed469eb39bbaeeda2
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 12:38:36 +0300

    Update LoginPage.js

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Login from '../components/auth/Login';
+import Modal from '../components/common/Modal';
+import '../../App.css';
+
+const LoginPage = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="page">
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Login onClose={() => setIsOpen(false)} />
+      </Modal>
+    </div>
+  );
+};
+
+export default LoginPage;
